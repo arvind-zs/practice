1571f04b262c0cbb956ab9eaaab90e3125e39227
Raramuri <arvind.yadav@zopsmart.com>
2022-09-01 12:11:47 +0530

    pushing stack file

--- stack/stack.go
+++ stack/stack.go
@@ -1,1 +1,23 @@
 package stack
+
+type Stack struct {
+	S1 []int
+}
+
+func New() Stack {
+	var s1 []int
+	return Stack{s1}
+}
+
+func (s *Stack) Push(ele int) {
+	s.S1 = append(s.S1, ele)
+}
+
+func (s *Stack) Pop() int {
+	if len(s.S1) != 0 {
+		lastElement := s.S1[len(s.S1)-1]
+		s.S1 = s.S1[:len(s.S1)-1]
+		return lastElement
+	}
+	return 0
+}
